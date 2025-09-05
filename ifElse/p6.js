bbb2e10294c0d7b958e07dbaa196be0974e9e65f
rakib <rakibulislamr83@gmail.com>
2025-09-05 18:06:16 +0600

    tenary operator

---
+++ ifElse/p6.js
@@ -0,0 +1,14 @@
+let invite = true;
+invite = true;
+let money = 1000;
+money=999
+if (invite == true) {
+  console.log("going to the party");
+  if (money >= 1000) {
+    console.log("buy a gift and go to party");
+  } else {
+    console.log("go to party without gift");
+  }
+} else {
+  console.log("Get out from friendlist");
+}
---
+++ ifElse/p7.js
@@ -0,0 +1,3 @@
+let shopping = 2500;
+let cashback = shopping >= 3000 ? 500 : 0;
+console.log (`you have ${cashback} cashback`);
